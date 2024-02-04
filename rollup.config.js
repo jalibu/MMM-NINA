4bfba84120b9d238b3bbb91fa0ce18a5e217d0b7
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2024-02-04 21:22:25 +0100

    Update rollup depedencies and process

--- rollup.config.js
+++ rollup.config.js
@@ -3,9 +3,9 @@ import commonjs from '@rollup/plugin-commonjs'
 import nodeResolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
 import json from '@rollup/plugin-json'
-import { terser } from 'rollup-plugin-terser'
+import terser from '@rollup/plugin-terser'
 
-const pkg = require('./package.json')
+import pkg from './package.json' assert { type: 'json' }
 
 const bannerText = `/*! *****************************************************************************
   ${pkg.name}
