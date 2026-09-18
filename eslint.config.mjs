3627810ec9f03c3a078ffb3bbedc2ac0e7224a31
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2026-09-18 21:09:51 +0200

    chore: modernize ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,43 @@
 // @ts-check
 import eslint from '@eslint/js'
-import { defineConfig } from 'eslint/config'
+import { defineConfig, globalIgnores } from 'eslint/config'
 import jsdoc from 'eslint-plugin-jsdoc'
 import tseslint from 'typescript-eslint'
 
 export default defineConfig(
-  eslint.configs.recommended,
-  tseslint.configs.strict,
-  tseslint.configs.stylistic,
-  jsdoc.configs['flat/recommended-typescript'],
+  globalIgnores(['MMM-NINA.js', 'node_helper.js', 'config.demo.js', '.test-dist/**']),
   {
-    ignores: ['MMM-NINA.js', 'node_helper.js', 'config.demo.js', '.test-dist/**']
+    files: ['src/**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      tseslint.configs.strictTypeChecked,
+      tseslint.configs.stylisticTypeChecked,
+      jsdoc.configs['flat/recommended-typescript']
+    ],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        sourceType: 'module',
+        ecmaVersion: 'latest'
+      }
+    },
+    rules: {
+      '@typescript-eslint/no-misused-promises': 'off',
+      '@typescript-eslint/no-unnecessary-condition': 'off',
+      '@typescript-eslint/no-unnecessary-type-conversion': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
+      '@typescript-eslint/restrict-template-expressions': ['error', { allowNumber: true }]
+    }
+  },
+  {
+    files: ['tests/**/*.mjs'],
+    extends: [eslint.configs.recommended],
+    languageOptions: {
+      sourceType: 'module',
+      ecmaVersion: 'latest'
+    }
   }
 )
